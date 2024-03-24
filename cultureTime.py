3294f77313298fb060b40b4fc5487b75fca433ab
JEANJACQUOT Thomas <thomas.jeanjacquot@telecomnancy.eu>
2024-03-24 18:27:23 +0100

    Ajout de la page home, profile, connexion et inscription à finir

--- cultureTime.py
+++ cultureTime.py
@@ -19,10 +19,18 @@ Session(app)
 def home():
     return render_template("home.html", title = "CultureTime")
 
-        
+@app.route("/sign-in")
+def sign_in():
+    return render_template("sign-in.html", title = "Sign In")
+
+@app.route("/sign-up")
+def sign_up():
+    return render_template("sign-up.html", title = "Sign Up")
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html", title = "Profile")
 
 if __name__ == "__main__":
-    if (False):
-        initDB()
-    app.run(debug=1, host='0.0.0.0', port='8080')
+    app.run(debug=True, host='0.0.0.0', port='8080')
 
